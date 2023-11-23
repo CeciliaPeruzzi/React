575b1a7aa53b76559ce0bc736b76576fd6627b32
Cecilia Peruzzi <ceciliaperuzzi2@hotmail.com>
2023-11-23 19:53:50 -0300

    api fetch

--- src/components/MoviesTable/MoviesInDb.jsx
+++ src/components/MoviesTable/MoviesInDb.jsx
@@ -4,10 +4,10 @@ import './moviesTable.css';
 function MoviesInDb (props) {
     return (
         <ul className='moviesTableGridRows'>
-            <li>{props.titulo}</li>
-            <li>{props.lenght}</li>
+            <li>{props.title}</li>
+            <li>lenght</li>
             <li>{props.rating}</li>
-            <li>{props.genres}</li>
+            <li>{props.genre ? props.genre.name : 'Sin Género'}</li>
             <li>{props.awards}</li>
         </ul>
     
--- src/components/MoviesTable/MoviesTableRow.jsx
+++ src/components/MoviesTable/MoviesTableRow.jsx
@@ -1,15 +1,35 @@
-import React from "react";
-import  { moviesInDb }  from '../../constants'
+import React, {Component} from "react";
 import MoviesInDb from "../../components/MoviesTable/MoviesInDb";
 import './moviesTable.css'
 
-function MoviesTableRow () {
-    return (
-        
-        moviesInDb.map( (movie, i) => <MoviesInDb key={movie + i} {...movie} />)
-    
 
-    )
-}
+
+class MoviesTableRow extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            moviesInDb: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/api/movies')
+        .then(respuesta => {
+            return respuesta.json()
+        })
+        .then(data => {
+            this.setState({moviesInDb: data.data})
+        })
+    }
+
+    render () {
+        return (
+            this.state.moviesInDb.map( (movie, i) => <MoviesInDb key={movie + i} {...movie} />)   
+            
+        )
+    }    
+
+    }
+
 
 export default MoviesTableRow;
